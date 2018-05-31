cfc26454b5c65843661975a98b142576de30057b
UlyssesWu <wdwxy12345@gmail.com>
2018-05-31 13:38:32 +0800

    Fix projects' information and remove unused things

--- UniFTPServerConsole/Program.cs
+++ UniFTPServerConsole/Program.cs
@@ -9,19 +9,21 @@ namespace UniFTPServerTestConsole
         {
             FtpServer f = new FtpServer(port:21,enableIPv6:true, ipv6Port:2121, logHeader:"UniFTP");
             f.Config = new FtpConfig( "D:\\Temp",welcome:new string[]{"By Ulysses"});
-            //如果有配置文件则可以读取
+            //Load configs...
             f.LoadConfigs();
-            //导入SSL证书
-            f.ImportCertificate("UniFTP.Open.pfx",null);
-            //绑定LOG事件，可以记录LOG
-            f.OnLog += sender => Console.WriteLine(((FtpLogEntry)sender).ToString());
-
-            f.Config.LogInWelcome = new string[]{"Welcome back,Commander."};
-            //如果没有则通过以下语句加入配置
+            //or add configs manually
             //f.AddUserGroup("test", AuthType.Password);
             //f.AddGroupRule("test", "/", "rwxrwxrwx");
             //f.AddUser("root", "test", "test");
-            //添加链接
+
+            //Import SSL cert
+            f.ImportCertificate("UniFTP.Open.pfx",null);
+            //Log event
+            f.OnLog += sender => Console.WriteLine(((FtpLogEntry)sender).ToString());
+
+            f.Config.LogInWelcome = new string[]{"Welcome back,Commander."};
+
+            //Add directory/file link
             f.AddLink("test", "M:\\ACGMusic", "/");
             f.AddGroupRule("test", "/Music", "r-xr-xr-x");
 
@@ -29,7 +31,7 @@ namespace UniFTPServerTestConsole
             Console.WriteLine("UniFTP Server Started!");
             Console.ReadLine();
             f.Stop();
-            //保存配置，下次就可以读取了
+            //Save configs for next time
             f.SaveConfigs();
             return;
         }
